33ed06c22e9d6b7f61667b81b575aa6ad6c90407
Nicklas Jensen <800751+Nillerr@users.noreply.github.com>
2022-03-28 15:32:09 +0200

    Make ViewModelProvider public

--- Sources/Swiftject/ViewModelProvider.swift
+++ Sources/Swiftject/ViewModelProvider.swift
@@ -5,11 +5,11 @@ import SwiftUI
  Enables creation of view models in views by lazily creating an instance of a
  view model as needed.
  */
-class ViewModelProvider<ViewModel: ObservableObject>: ObservableObject {
+public class ViewModelProvider<ViewModel: ObservableObject>: ObservableObject {
     private var viewModel: ViewModel?
     private var subscription: AnyCancellable?
 
-    func get(orCreate initializer: () -> ViewModel) -> ViewModel {
+    public func get(orCreate initializer: () -> ViewModel) -> ViewModel {
         if let viewModel = viewModel {
             return viewModel
         }
@@ -27,13 +27,17 @@ class ViewModelProvider<ViewModel: ObservableObject>: ObservableObject {
 
 extension ViewModelProvider {
     /**
-     Enables injection of a view model resolved form a ``Resolver`` in a view.
+     Enables injection of a view model resolved from a ``Resolver`` in a view.
      */
-    func get(from resolver: Resolver) -> ViewModel {
+    public func get(from resolver: Resolver) -> ViewModel {
         get { resolver.resolve() }
     }
 
-    func get<Factory>(from resolver: Resolver, using factoryType: Factory.Type, factory: (Factory) -> ViewModel) -> ViewModel {
+    /**
+     Enables injection of a view model resolved from a ``Resolver`` using a
+     factory in a view.
+     */
+    public func get<Factory>(from resolver: Resolver, using factoryType: Factory.Type, factory: (Factory) -> ViewModel) -> ViewModel {
         get { factory(resolver.resolve(Factory.self)) }
     }
 }
